0a45c22d0134f0db5c077d336262b7275bb0d860
Stephen Powell <steve.powell@edgewordstraining.co.uk>
2018-09-11 20:36:56 +0100

    Made some changes

--- functionlib.js
+++ functionlib.js
@@ -1,12 +1,13 @@
 const { By, Key, until } = require('selenium-webdriver');
-
+//Something new
 async function waitUtil(driver, locator, timeout){
     try {
         let el = await driver.wait(until.elementLocated(locator),timeout);
         return el;
     } catch (error) {
+
         throw new Error(`Didn't find ${locator.toString} in given timeout ${timeout}`)
-    }
+    }//something else
     
 }
 
